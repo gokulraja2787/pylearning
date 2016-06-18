51b4165f083b1020eee2c743e10e409e5b1eca4c
Gokul Rangarajan <gokulraja2006@gmail.com>
2016-06-18 19:19:37 -0400

    Update PhoneBook module.

--- datalib/phoneBook.py
+++ datalib/phoneBook.py
@@ -4,7 +4,7 @@
     Author: Gokul Rangarajan
     Description:    Module to handle Phone book
 """
-from iolib.iputil import acceptint
+from iolib.iputil import acceptint, acceptstring
 
 book = []
 NAME = 'Name'
@@ -14,23 +14,37 @@ CELL = 'Cell'
 def addcontact(name, phone):
     entry = {NAME: name, CELL: phone}
     book.append(entry)
-    pass
 
 
-def countContact():
+def countcontacts():
     return len(book)
+
+
+def listcontacts():
+    for b in book:
+        print("%s : %s\n" % (b[NAME], b[CELL]))
+
+
 
 def phonebook():
     print("\n\t\tEntering Phone book")
     print("\t\t~~~~~~~~~~~~~~~~~~~")
-    print("\t\t 1.  Add a contact")
-    print("\t\t 2.  Display phone book")
-    print("\t\t 3.  Count contact")
-    print("\t\t 0.  Exit PhoneBook")
     menu = None
     while (None == menu or menu != 0):
+        print("\t\t 1.  Add a contact")
+        print("\t\t 2.  Display phone book")
+        print("\t\t 3.  Count contact")
+        print("\t\t 0.  Exit PhoneBook")
         mnu = 'Select operation:'
         menu = acceptint(mnu)
-        if 3 == menu:
-            print("Size of PhoneBook is: %d" % len(book))
+        if 1 == menu:
+            mnu = 'Enter the Name: '
+            name = acceptstring(mnu)
+            mnu = 'Enter the Phone Number:'
+            phone = acceptint(mnu)
+            addcontact(name, phone)
+        elif 2 == menu:
+            listcontacts()
+        elif 3 == menu:
+            print("Size of PhoneBook is: %d" % countcontacts())
     return
